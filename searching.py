f6f90ae8b8cffbb003e84f9a41bf4b3fa9d286bc
Matej Karasek <karasmatej@email.cz>
2024-04-10 10:39:50 +0200

    patttern

--- searching.py
+++ searching.py
@@ -17,17 +17,6 @@ def read_data(file_name, field):
         data = json.load(json_file)
 
 def linear_search(sekvence,cislo):
-    """
-    slovnik = [] # reseults{"positions":[],"count":0}, results["positions"].appent(i)
-    count = 0
-    for i in range(len(sekvence)):
-        if sekvence[i] == cislo:
-            slovnik.append(sekvence[i])
-            count += 1
-        else:
-            continue
-    return (slovnik, count)
-    """
     results ={"positions": [], "count": 0}
 
     for i in range(len(sekvence)):
@@ -38,10 +27,56 @@ def linear_search(sekvence,cislo):
             continue
     return results
 
+def pattern_search(sequence, pattern):
+#  "dna_sequence": "ATGACGGAATATAAGC.."
+    idx = 0
+    position = set()
+    pattern_index = 0
+    n = len(sequence)
+    m = len(pattern)
+    while idx < m:
+        if sequence[idx + pattern_index] != pattern[pattern_index]:
+            break
+        pattern_index = pattern_index + 1
+    if pattern_index == m:
+        position.add(idx + m //2)
+        idx += 1
+    return position
+
+
+
+
+
 def main():
+
+    sekvence = json.load("dna_sequence")
+    pattern = "ATA"
     data1 = read_data("sequential.json","unordered_numbers")
     print(data1)
 
+
+
+
+
 if __name__ == '__main__':
     main()
-    cislo = 10
+    cislo = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
