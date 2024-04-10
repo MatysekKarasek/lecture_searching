574f5db385cc7baf6f3472f4c85b6f83eb4ddbf0
Matej Karasek <karasmatej@email.cz>
2024-04-10 08:48:30 +0200

    add function

--- searching.py
+++ searching.py
@@ -11,12 +11,15 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+    cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, mode="r") as json_file:
+        data = json.load(json_file)
 
 
 def main():
-    pass
-
+    data1 = read_data("sequential.json","unordered_numbers")
+    print(data1)
 
 if __name__ == '__main__':
     main()
